8ed97935a0808e4eb64369e36db791a47fe63cab
chengtianle1997 <chengtianle1997@163.com>
2021-01-06 15:44:51 +0800

    lecture 14 slide added

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,11 +21,16 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
-
+for i = 1:size(X, 1)
+    closest_dist = inf;
+    for j = 1:K
+        dist = sum((X(i, :) - centroids(j, :)).^2);
+        if  dist < closest_dist
+            closest_dist = dist;
+            idx(i) = j;
+        end
+    end
+end
 
 % =============================================================
 
--- machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -13,12 +13,8 @@ centroids = zeros(K, size(X, 2));
 %               the dataset X
 %
 
-
-
-
-
-
-
+rand_index = randperm(size(X, 1), K);
+centroids = X(rand_index, :);
 
 % =============================================================
 
